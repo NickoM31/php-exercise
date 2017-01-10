1853f3fb88cc094fba895f0381ee5b256cf37887
Nicko <nicolasmathieu31@hotmail.com>
2017-01-10 11:05:19 +0100

    Ajout exercice 2 array

--- array/exercice2.php
+++ array/exercice2.php
@@ -2,14 +2,14 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<title>Exercice1</title>
+	<title>Exercice2</title>
 </head>
 <body>
 	<h1>Array</h1>
 	<?php
 	$monthTab = array("janvier","février","mars","avril","mai","juin",
 		"juillet","aout","septembre","octobre","novembre","décembre");
-	var_dump($monthTab) ;
+	echo $monthTab[2];
 
 	?>
 </body>
