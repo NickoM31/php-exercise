e2f35d91c46b0390f37612c15943b5ed95647383
Nicko <nicolasmathieu31@hotmail.com>
2017-01-10 18:38:42 +0100

    Ajout du dossier form ainsi que l'exercice1 form

---
+++ form/form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Formulaire</title>
+</head>
+<body>
+	<form action="">
+		<label for="prenom">Prénom</label>
+		<input type="" name="prenom" id="prenom" placeholder="Votre prénom">
+		<label for="nom">Nom</label>
+		<input type="text" name="nom" id="nom" placeholder="Votre nom">
+		<button><a href="user.php?nom=nicko&wamp;nom=mi"> Valider</a></button>
+
+
+	</form>
+</body>
+</html>
