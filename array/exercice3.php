59c5830c9c63801cc901cbb85c67a0fdf10fab4f
Nicko <nicolasmathieu31@hotmail.com>
2017-01-10 11:10:49 +0100

    Ajout exercice 3 array

---
+++ array/exercice3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercice3</title>
+</head>
+<body>
+	<h1>Array</h1>
+	<?php
+	$monthTab = array("janvier","février","mars","avril","mai","juin",
+		"juillet","aout","septembre","octobre","novembre","décembre");
+	echo " La 5eme valeur du tableau est le mois de $monthTab[4].";
+	?>
+</body>
+</html>
