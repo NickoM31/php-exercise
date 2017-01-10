3c31ed1432c4c58d0a51b4ff26c51fd7b23515ad
Nicko <nicolasmathieu31@hotmail.com>
2017-01-10 10:38:28 +0100

    Ajout dossier array et exercice 1 array

---
+++ array/exercice1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercice1</title>
+</head>
+<body>
+	<h1>Array</h1>
+	<?php
+	$monthTab = array("janvier","février","mars","avril","mai","juin",
+		"juillet","aout","septembre","octobre","novembre","décembre");
+	var_dump($monthTab) ;
+
+	?>
+</body>
+</html>
