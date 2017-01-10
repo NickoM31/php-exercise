6242e6aab6eca8f1ba1e73d968cfd11a15dbb746
Nicko <nicolasmathieu31@hotmail.com>
2017-01-10 22:42:51 +0100

    Ajout exercice 5 form

---
+++ form/exercice5/user.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercice5</title>
+</head>
+<body>
+<h1>Formulaire</h1>
+<?php 
+
+// if (isset($_POST["choix"]) && isset($_POST["prenom"]) && isset($_POST["nom"])) {
+// 	echo "Bienvenue " . $_POST["choix"] ." ". $_POST["prenom"] ." ". $_POST["nom"] . "!";
+// }
+
+
+ ?>
+	
+</body>
+</html>
