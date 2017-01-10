b79bc6b74713d1e8414c7979f7e1f3068ff48262
Nicko <nicolasmathieu31@hotmail.com>
2017-01-10 12:24:31 +0100

    Ajout exercice 6 array

---
+++ array/exercice6.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercice6</title>
+</head>
+<body>
+	<h1>Array</h1>
+	<?php
+	$monthTab = array("02"=>"Aisne","59"=>"Nord","60"=>"Oise",
+		"62"=>"Pas-de-Calais","80"=>"Somme");
+		echo "La valeur de l'index 59 est $monthTab[59].";
+		?>
+	</body>
+	</html>
