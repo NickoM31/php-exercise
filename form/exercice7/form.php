8afe128a21f35716b59a5e73ddaf1f436229611a
Nicko <nicolasmathieu31@hotmail.com>
2017-01-12 19:22:05 +0100

    Changement sur exercice 7 pull home

--- form/exercice7/form.php
+++ form/exercice7/form.php
@@ -6,29 +6,42 @@
 </head>
 <body>
 	<h1>formulaire</h1>
+	<!-- <?php 	
+	// if (empty($_POST)){
+	// 	echo "Veuiller remplir tous les champs, merci.";
+	?> -->
+
+	<form action="form.php" method="post" >
+		<select name="choix" id="">
+			<option></option>
+			<option value="madame">Mme</option>
+			<option value="monsieur">Mr</option>
+		</select>
+		<label for="prenom">Prénom</label>
+		<input type="" name="prenom" id="prenom" placeholder="Votre prénom">
+		<label for="nom">Nom</label>
+		<input type="text" name="nom" id="nom" placeholder="Votre nom">
+		<!-- <input type="file" name="upload"> -->
+
+		<input type="submit" name="valider">
+	</form>
+	<!-- $_FILES["upload"]["name"] ." ".  -->
+
 	<?php 
-	if (empty($_POST)){
-		?>
-		<form action="form.php" method="Post" enctype="multipart/form-data">
-			<select name="choix" id="">
-				<option value="madame">Mme</option>
-				<option value="monsieur">Mr</option>
-			</select>
-			<label for="prenom">Prénom</label>
-			<input type="" name="prenom" id="prenom" placeholder="Votre prénom">
-			<label for="nom">Nom</label>
-			<input type="text" name="nom" id="nom" placeholder="Votre nom">
-			<input type="file" name="upload">
-			<button>Valider</button>
-		</form>
-		<?php 
-	}else{
-		if (isset($_POST["choix"]) && isset($_POST["prenom"]) && isset($_POST["nom"]) && isset($_POST["upload"])) {
-			echo $_POST["upload"] ." ". "Bienvenue " . $_POST["choix"] ." ". $_POST["prenom"] ." ". $_POST["nom"] . "!";
-		}else{
-			echo "Veuiller remplir tous les champs, merci.";
-		}
+
+	if (isset($_POST["valider"])) {
+		$choix = $_POST["choix"];
+		$prenom = $_POST["prenom"];
+		$nom = $_POST["nom"];
+		echo "Bonjour " . $choix ." ".$prenom." ".$nom . " !"; 
+	}elseif (empty($_POST)) {
+			var_dump($_POST);
+		
+		echo "buggg";
 	}
+	// }else{
+	// 	echo "Bienvenue " . $_POST["choix"] ." ". $_POST["prenom"] ." ". $_POST["nom"] . "!";
+	// }
 	?>
 
 </body>
