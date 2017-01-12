ec9e2ddb816d5219fe5d4944e6c32257ced0d2b3
Nicko <nicolasmathieu31@hotmail.com>
2017-01-12 13:57:21 +0100

    Ajout exercice 2 globalVariable

---
+++ globalVariable/exercice2/index.php
@@ -0,0 +1,28 @@
+<?php  session_start(); 
+$_SESSION["nom"] = 'Mathieu' ;
+$_SESSION["prenom"] ='Nicko' ;
+$_SESSION["age"] = '37';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercice 2</title>
+</head>
+<body>
+
+	<h1>globalVariable</h1>
+
+	<a href="user.php?nom=nom&prenom=prenom&age=age ">Lien</a>
+
+
+	<?php
+	if (!isset($_SESSION['count'])) {
+		$_SESSION['count'] = 0;
+	} else {
+		$_SESSION['count']++;
+	}
+	?>
+
+</body>
+</html>
---
+++ globalVariable/exercice2/user.php
@@ -0,0 +1,15 @@
+<?php  session_start(); 
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+<span>Je me souviens de toi ! Tu t'appelles <?php echo $_SESSION['prenom'] . ' ' . $_SESSION['nom'] ; ?> et tu as <?php echo $_SESSION["age"] ?> ans.</span>
+
+	<a href="index.php">Home</a>
+</body>
+</html>
