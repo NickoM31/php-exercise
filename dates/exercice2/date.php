eae8dfc24771c75835834f0a038815bcd901935d
Nicko <nicolasmathieu31@hotmail.com>
2017-01-13 11:04:42 +0100

    Ajout exercice 2 date

---
+++ dates/exercice2/date.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercice1</title>
+</head>
+<body>
+	<h1>Les dates</h1>
+
+	<?php 
+	$dateNow = date("d-m-Y");
+	echo $dateNow;
+	?>
+
+
+</body>
+</html>
