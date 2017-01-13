072b238ae83234cd2ec644ede971e92a12e31e93
Nicko <nicolasmathieu31@hotmail.com>
2017-01-13 15:39:03 +0100

    Ajout exercice 4 dates

---
+++ dates/exercice4/date.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercice4</title>
+</head>
+<body>
+	<h1>Les Dates Timestamp</h1>
+	<?php
+	$convertTime = "%A %d %B %Y %H:%M";
+	setlocale (LC_TIME, 'fr_FR.utf8','fra');
+	?>
+	<div><?php echo strftime( $convertTime, time('UTC')); ?></div>
+	<div><?php echo strftime($convertTime ,mktime(15,0,0,8,2,2016)); ?></div>
+</body>
+</html>
